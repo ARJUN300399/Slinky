00e920538857d3cc3ada3d80a267552550ddd3d2
Arjun <arj300399@gmail.com>
2025-01-11 20:14:11 +0530

    first commit

---
+++ slinky-backend/src/main/java/dev/arjun/slinky_backend/SlinkyBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.arjun.slinky_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlinkyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlinkyBackendApplication.class, args);
+	}
+
+}
---
+++ slinky-backend/src/test/java/dev/arjun/slinky_backend/SlinkyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arjun.slinky_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlinkyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
